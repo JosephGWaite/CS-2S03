9ad6338068d6976259f1c247940083ef147785fb
Joseph Waite <jw@waitefarms.ca>
2015-09-16 11:15:02 -0400

    intital commit from LXDE VM on surface

---
+++ HW1/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class HelloWorld {
+	public static void main(String[] args){
+		int len = args.length;
+		String output = "";
+		for(int i = len -1 ; i >= 0 ; i--){
+				output += args[i]
+		}
+		system.log
+	}
+}
