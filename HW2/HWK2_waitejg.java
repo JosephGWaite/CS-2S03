22822740483a6ef7ee494f372548d4ca69951f27
Joseph Waite <jw@waitefarms.ca>
2015-09-25 12:33:11 -0400

    ungoofed the for loop in multi(). It compiles and works

--- HW2/HWK2_waitejg.java
+++ HW2/HWK2_waitejg.java
@@ -6,25 +6,34 @@ Student Number: 1403712
 Description: naive matrix multiplication in java.  O(n^3)
 */
 
+import java.util.Arrays;
+
+
 class HWK2_waitejg {
 
 	public static void main(String[] args){ //main method
 		//TODO
 		//Take args and make them into matricies
+		//TEST CASES?? 
 
-		int matrixA[][]={{50,60,55}
-						,{62,65,70}
-						,{72,66,77}};
 
-		int matrixB[][]={{50,60,55}
-						,{62,65,70}
-						,{72,66,77}};
+		double matrixA[][] = {{50,60,55}
+							,{62,65,70}
+						    ,{72,66,77}};
+
+		double matrixB[][] = {{50,60,55}
+							,{62,65,70}
+							,{72,66,77}};
+		double ans[][]=  multi(matrixA, matrixB);
+		System.out.println(Arrays.deepToString(ans));
+
+
 
 	}
 
 	public static double[][] multi(double matrixA[][], double matrixB[][]) { //multiplication method
 		/* 
-		//Naive algorithm (from wikipedia)
+		//Naive matrix multiplication algorithm (from wikipedia)
 
     	Input: matrices A[n][m] and B[m][p]
    		Let C be a new matrix of [n][p] size
@@ -35,28 +44,41 @@ class HWK2_waitejg {
             Set C[i][j] ← sum
     	Return C
 		*/ 
-
+    	System.out.println(Arrays.deepToString(matrixA));
+    	System.out.println(Arrays.deepToString(matrixB));
     	int n = matrixA[0].length; //length of first nested element, basically the width
     	int m = matrixB.length; //
-    	int p = matrixB[0].length; 
-
+    	int p = matrixB[0].length; //
+    	System.out.println(n + " " + m + " " + p);
     	if (m != matrixA.length) { //If dimensionons are invalid.
     		throw new Error("Multiplication Error"); 
     	}
 
-		double[][] matrixC = new int[m][p];
-		for(int i =0; i < n ; i++){
-			for (int j=0; i<n ; j++){
-				for(int k=0;k<n;k++){
+		double[][] matrixC = new double[m][p];
+		for(int i =0; i < m; i++){
+			for (int j=0; j < p; j++){
+				for(int k=0; k < n; k++){
+					System.out.println("i: " + i + " j: " + j + " k: " + k);
 					matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
 				}
 			}
 		}
-		return matrixC
+		return matrixC;
 	}
 
 	public static boolean isInvertible(double matrix[][]){
+		// is is square? 
+		// non square matricies do not have an inverse
+		// 
+		return true ;
+	}
 
+	public static double[][] inverse(double matrix[][]){ 
+		// fuck this is hard
+		// LU Decomposition
+		// http://www.econ.umn.edu/undergrad/math/An%20Algorithm%20for%20Finding%20the%20Inverse.pdf
+		// fuck
+		return matrix;
 	}
 
 }
