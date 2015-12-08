b082964f9e523f7777dc91f557435051f3040d7c
Joseph Waite <jw@waitefarms.ca>
2015-12-07 19:27:42 -0500

    token precedence changes

--- HW6/token.h
+++ HW6/token.h
@@ -3,16 +3,16 @@
 
 #include <string>
 
-//Value is basically precedent. 
-// it's used in the 
-int const open_token = 7;  // (
-int const close_token = 7; // (
-int const add_token = 5;   // + 
-int const sub_token = 5;   // - 
-int const div_token = 6;   // / 
+// Value is not precedent. This has been flipped around a couple times
+// precedence is determined 
+int const open_token = 1;  // (
+int const close_token = 2; // (
+int const add_token = 3;   // + 
+int const sub_token = 4;   // - 
+int const div_token = 5;   // / 
 int const mult_token = 6;  // * 
-int const num_token = 1;   // 1,2,3,4,5,6,7,8,9,
-int const eol_token =7;
+int const num_token = 7;   // 1,2,3,4,5,6,7,8,9,
+int const eol_token =8;
  
 struct Token { 
 	int type; 
