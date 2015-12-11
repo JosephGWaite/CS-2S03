e10806a87464fd63c4a443ebf79718da62bb506b
Joseph Waite <jw@waitefarms.ca>
2015-12-11 18:22:24 -0500

    eval example in division

--- HW6/Division.cpp
+++ HW6/Division.cpp
@@ -1,9 +1,12 @@
 #include <cstring> 
 class Divide : public ArithmeticExpression{
-	string evaluate() {}
+	
+	string evaluate() {
+		return this->evaluate(this->right) / evaluate(this->right)
+	}
 
 	void print(){} //left `/` right
 	float convert (string s) { 
 		//Converts String (returned by eval to float) 
 	}
-}
+};
